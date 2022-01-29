40e4a192ca28446b8e1b165f3b3c6b4a95f6955f
alepereira <naitwa-alexandre@gmail.com>
2022-01-29 11:42:05 -0300

    dia 29/01

--- heroku/example-01/index.js
+++ heroku/example-01/index.js
@@ -4,8 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/ping', (_req, res) => {
-  res.status(200).json({ message: 'pong' });
+const html = `<h2>IT WORKS!</h2>`;
+
+app.get('', (_req, res) => {
+  res.status(200).send(html);
 });
 
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
--- sequelize-example02/index.js
+++ sequelize-example02/index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
