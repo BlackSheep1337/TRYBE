a99486913010939a566307cd9d1066aa4b498316
alepereira <naitwa-alexandre@gmail.com>
2022-02-02 14:23:28 -0300

    migrations and seeds

---
+++ app-with-sequelize/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const userController = require('./controllers/userController');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use('/users', userController);
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
